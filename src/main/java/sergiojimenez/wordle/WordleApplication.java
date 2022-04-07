84485ab47d6f86e783fb74241b5a7d60f0ae9598
SergioJimenezR <sergio.membry@gmail.com>
2022-04-07 20:01:48 +0200

    Innecesariedad

--- src/main/java/sergiojimenez/wordle/WordleApplication.java
+++ src/main/java/sergiojimenez/wordle/WordleApplication.java
@@ -3,9 +3,7 @@ package sergiojimenez.wordle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.core.Ordered;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @SpringBootApplication
 @EnableAsync
@@ -13,12 +11,6 @@ public class WordleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WordleApplication.class, args);
-	}
-
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("forward:/index.html");
-		registry.addViewController("/error").setViewName("forward:/");
-		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 	}
 
 }
